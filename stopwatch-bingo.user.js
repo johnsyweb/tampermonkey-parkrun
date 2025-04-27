4455a3fd6446aa0058e7092241969b7e782e8cac
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2025-04-27 10:34:57 +0000

    chore: update userscript versions [skip ci]

--- stopwatch-bingo.user.js
+++ stopwatch-bingo.user.js
@@ -34,7 +34,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/stopwatch-bingo.user.js
-// @version      2025-04-22
+// @version      2025-04-27
 // ==/UserScript==
 
 (function () {
