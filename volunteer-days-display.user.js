c809fdd3c8e73973e781306f4a071ec7338d6288
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2025-07-06 18:19:47 +0000

    chore: update userscript versions [skip ci]

--- volunteer-days-display.user.js
+++ volunteer-days-display.user.js
@@ -33,7 +33,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/volunteer-days-display.user.js
-// @version      2025-06-16 01:53 02:17 01:28 02:13 19:55 10:42
+// @version      2025-07-06 01:53 02:17 01:28 02:13 19:55 10:42
 // ==/UserScript==
 
 (function () {
