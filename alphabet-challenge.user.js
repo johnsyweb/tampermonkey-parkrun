000431f22bc683483ac73d8a452206db793afff0
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2025-11-03 09:58:05 +0000

    chore: update userscript versions [skip ci]

--- alphabet-challenge.user.js
+++ alphabet-challenge.user.js
@@ -34,7 +34,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/alphabet-challenge.user.js
-// @version      1.0.28
+// @version      1.0.29
 // ==/UserScript==
 
 (function () {
