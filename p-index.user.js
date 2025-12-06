93d22207cc2676ac2332646e3717bfb3902455ea
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2025-12-06 10:50:29 +0000

    chore: update userscript versions [skip ci]

--- p-index.user.js
+++ p-index.user.js
@@ -34,7 +34,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/p-index.user.js
-// @version      1.0.51
+// @version      1.0.52
 // ==/UserScript==
 
 (function () {
