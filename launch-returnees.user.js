be2017bae8404c6e665dc0076583d99488d1ed01
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-01-28 00:41:50 +0000

    chore: update userscript versions [skip ci]

--- launch-returnees.user.js
+++ launch-returnees.user.js
@@ -33,7 +33,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/launch-returnees.user.js
-// @version      1.0.67
+// @version      1.0.68
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 // Built: 2026-01-27T23:51:00.906Z
