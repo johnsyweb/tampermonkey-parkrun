6ee4b8385959cd3ed3c260bc7b6acd54518289bf
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-02-03 08:40:07 +0000

    chore: update userscript versions [skip ci]

--- visited-countries.user.js
+++ visited-countries.user.js
@@ -33,7 +33,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/visited-countries.user.js
-// @version      1.0.65
+// @version      1.0.66
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 
