afa331a1323cbd87f72ddb7d0623b42b76b2db38
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-02-04 01:40:37 +0000

    chore: update userscript versions [skip ci]

--- parkrun-walker-analysis.user.js
+++ parkrun-walker-analysis.user.js
@@ -35,7 +35,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/parkrun-walker-analysis.user.js
-// @version      1.0.65
+// @version      1.0.66
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 
