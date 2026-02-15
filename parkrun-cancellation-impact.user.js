df7d8ef455bef10b8361af94f24e22062237af7f
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-02-15 12:51:23 +0000

    chore: update userscript versions [skip ci]

--- parkrun-cancellation-impact.user.js
+++ parkrun-cancellation-impact.user.js
@@ -34,7 +34,7 @@
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/parkrun-cancellation-impact.user.js
 // @require      https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
-// @version      0.1.8
+// @version      0.1.9
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 
