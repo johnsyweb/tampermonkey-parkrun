d49a2ecf0b9cb71c585b6b798bd6f8c57cb36ac4
Pete Johns <paj+github@johnsy.com>
2026-02-15 23:23:24 +1100

    chore: use single .tool-versions with mise, ignore docs/vendor in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { ignores: ['*.user.js', 'dist/**', 'docs/images/**'] },
+  { ignores: ['*.user.js', 'dist/**', 'docs/images/**', 'docs/vendor/**'] },
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
