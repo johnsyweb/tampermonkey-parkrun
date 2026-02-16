9b8e781e3e48bb83f6d4cae7403433fc39f3c410
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-02-16 09:24:41 +0000

    chore: update userscript versions [skip ci]

--- junior-group-sunday-link.user.js
+++ junior-group-sunday-link.user.js
@@ -33,7 +33,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/junior-group-sunday-link.user.js
-// @version      0.2.0
+// @version      0.2.1
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 
