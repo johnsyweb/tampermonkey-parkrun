fa107f3dfde73153088a4d56bedd1d6f3a229dec
Pete Johns <paj+github@johnsy.com>
2026-04-25 21:30:17 +1000

    feat(p-index): add progression chart

--- __tests__/p-index.test.js
+++ __tests__/p-index.test.js
@@ -1,4 +1,10 @@
-const { calculatePIndex } = require('../src/p-index.user');
+const {
+  buildDifficultySummary,
+  calculatePIndex,
+  calculatePIndexProgression,
+  extractFinishTimeline,
+  groupFinishesByEvent,
+} = require('../src/p-index.user');
 
 describe('calculatePIndex', () => {
   test('calculates p-index for sample event details', () => {
@@ -86,5 +92,83 @@ describe('calculatePIndex', () => {
         'Aurora (2): 21/02/2023 (#76) - 28/02/2023 (#77)',
       ])
     );
+  });
+});
+
+describe('progression analytics', () => {
+  test('extractFinishTimeline sorts finishes by date ascending', () => {
+    document.body.innerHTML = `
+      <table id="results">
+        <tbody>
+          <tr>
+            <td><a>Alpha</a></td>
+            <td>21/01/2023</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td><a>Bravo</a></td>
+            <td>07/01/2023</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td><a>Alpha</a></td>
+            <td>14/01/2023</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    `;
+
+    const table = document.querySelector('#results');
+    const timeline = extractFinishTimeline(table);
+
+    expect(timeline.map((finish) => finish.date)).toEqual([
+      '07/01/2023',
+      '14/01/2023',
+      '21/01/2023',
+    ]);
+  });
+
+  test('calculatePIndexProgression marks increase points with gap sizes', () => {
+    const finishTimeline = [
+      { eventName: 'A', date: '07/01/2023', eventNumber: '1' },
+      { eventName: 'B', date: '14/01/2023', eventNumber: '1' },
+      { eventName: 'A', date: '21/01/2023', eventNumber: '2' },
+      { eventName: 'B', date: '28/01/2023', eventNumber: '2' },
+      { eventName: 'A', date: '04/02/2023', eventNumber: '3' },
+    ];
+
+    const progression = calculatePIndexProgression(finishTimeline);
+
+    expect(progression.map((point) => point.pIndex)).toEqual([1, 1, 1, 2, 2]);
+    expect(progression.filter((point) => point.isJump).map((point) => point.finishes)).toEqual([
+      1, 4,
+    ]);
+    expect(
+      progression
+        .filter((point) => point.isJump)
+        .map((point) => point.finishesSincePreviousIncrease)
+    ).toEqual([1, 3]);
+  });
+
+  test('buildDifficultySummary returns extended difficulty metrics', () => {
+    const finishTimeline = [
+      { eventName: 'A', date: '07/01/2023', eventNumber: '1' },
+      { eventName: 'B', date: '14/01/2023', eventNumber: '1' },
+      { eventName: 'A', date: '21/01/2023', eventNumber: '2' },
+      { eventName: 'B', date: '28/01/2023', eventNumber: '2' },
+      { eventName: 'A', date: '04/02/2023', eventNumber: '3' },
+    ];
+    const progression = calculatePIndexProgression(finishTimeline);
+    const grouped = groupFinishesByEvent(finishTimeline);
+    const pIndex = calculatePIndex(grouped).pIndex;
+
+    const summary = buildDifficultySummary(progression, pIndex);
+
+    expect(summary).toEqual([
+      'Current p-index: 2',
+      'Finishes since previous increase: 1',
+      'Longest gap: 3 finishes (between p-index 1 and 2)',
+    ]);
   });
 });
