bccd2eb2b2ce0133f9e3ed0a91d9d78e1b3cf1a2
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-06-21 22:25:55 +0000

    chore: update userscript versions [skip ci]

--- p-index-progression.user.js
+++ p-index-progression.user.js
@@ -35,14 +35,18 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/p-index-progression.user.js
-// @version      1.0.7
+// @version      1.0.8
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 
 (() => {
   var __getOwnPropNames = Object.getOwnPropertyNames;
   var __commonJS = (cb, mod) => function __require() {
-    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+    try {
+      return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+    } catch (e) {
+      throw mod = 0, e;
+    }
   };
 
   // lib/p-index-core.js
--- src/future-roster-printable.user.js
+++ src/future-roster-printable.user.js
@@ -37,7 +37,7 @@
 // @screenshot-timeout   8000
 // @screenshot-viewport  1200x800
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/future-roster-printable.user.js
-// @version      0.1.5
+// @version      0.1.6
 // ==/UserScript==
 
 const STYLE_ID = 'parkrun-future-roster-printable';
