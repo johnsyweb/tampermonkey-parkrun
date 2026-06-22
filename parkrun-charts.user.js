e13caaccda50a3c441637f6e79d5a5cd09712446
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-06-22 00:35:03 +0000

    chore: update userscript versions [skip ci]

--- parkrun-charts.user.js
+++ parkrun-charts.user.js
@@ -35,7 +35,7 @@
 // @supportURL   https://github.com/johnsyweb/tampermonkey-parkrun/issues/
 // @tag          parkrun
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/parkrun-charts.user.js
-// @version      1.1.16
+// @version      1.1.17
 // ==/UserScript==
 // DO NOT EDIT - generated from src/ by scripts/build-scripts.js
 
