738785996afe1d634c10916e03f6a42275b12afb
johnsyweb <39911+johnsyweb@users.noreply.github.com>
2026-09-17 06:53:02 +0000

    chore: update userscript versions [skip ci]

--- src/future-roster-printable.user.js
+++ src/future-roster-printable.user.js
@@ -37,7 +37,7 @@
 // @screenshot-timeout   8000
 // @screenshot-viewport  1200x800
 // @updateURL    https://raw.githubusercontent.com/johnsyweb/tampermonkey-parkrun/refs/heads/main/future-roster-printable.user.js
-// @version      0.2.0
+// @version      0.2.1
 // ==/UserScript==
 
 const STYLE_ID = 'parkrun-future-roster-printable';
